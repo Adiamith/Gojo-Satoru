844169862619fda2b6ea32471d7139f52a858c49
Adiamith <97206490+Adiamith@users.noreply.github.com>
2022-07-03 12:41:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,17 +20,17 @@ global.APIKeys = {
 
 //customize settings\\
 global.owner = ['917561852787']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.premium = ['917561852787']
+global.ownernomer = '917561852787'
 global.ownername = 'i am flash'
 global.botname = 'Flash bot'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://github.com/Adiamith/Gojo-Satoru'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
+global.sc = 'https://github.com/Adiamith/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Flash'
+global.author = 'i am flash'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
